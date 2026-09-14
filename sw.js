feb49ef1903441cde256b3c6208a8d8a4fee61a3
mixr11d <mix.r11d@gmail.com>
2026-09-14 10:38:41 +0300

    Add service worker for cache cleaning and self-destruct
    Implements a service worker that cleans cache and unregisters itself.

---
+++ sw.js
@@ -0,0 +1,22 @@
+/**
+ * Service Worker Cache Cleaner & Self-Destruct
+ */
+self.addEventListener('install', function () {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    }).then(function () {
+      return self.registration.unregister();
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
